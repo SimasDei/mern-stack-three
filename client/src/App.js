9616d2337bffdde5249ba427846ce4bfa93a319e
SimasDei <simondeiman@gmail.com>
2018-12-23 13:36:08 +0200

    add semantic ui

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
 
 // Use appJS as a Route Dispatcher
 const App = () => (
-  <div>
+  <div className="ui container">
     <Route path="/" exact component={HomePage} />
+    <Route path="/login" exact component={LoginPage} />
   </div>
 );
 
--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -1,3 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default () => <div>s</div>;
+const Homepage = () => (
+  <div>
+    <h1>Home Page</h1>
+    <Link to="/login">Login</Link>
+  </div>
+);
+
+export default Homepage;
