4710e29ac4d95d967ae4ee6b7196bc5dda60ea1e
SimasDei <simondeiman@gmail.com>
2018-12-24 22:14:01 +0200

    add authorized routing, email confirmation

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
+import DashboardPage from './components/pages/DashboardPage';
+import UserRoute from './components/routes/UserRoute';
 
 // Use appJS as a Route Dispatcher
 const App = () => (
   <div className="ui container">
     <Route path="/" exact component={HomePage} />
     <Route path="/login" exact component={LoginPage} />
+    <UserRoute path="/dashboard" exact component={DashboardPage} />
   </div>
 );
 
